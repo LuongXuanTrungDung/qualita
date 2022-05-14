485b37bd30ae899ae54c742d2f9aff6794fb33c6
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2022-05-14 13:44:45 +0700

    Setup: Replace Bootstrap with MUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Home() {
     return (
-        <main></main>
+        <React.Fragment>
+            <main></main>
+        </React.Fragment>
     );
 }
