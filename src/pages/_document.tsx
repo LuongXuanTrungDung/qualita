8d8ba84ee2086d6e3499740e9dce56a200b7e2a9
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2022-05-18 18:05:26 +0700

    Setup: Head's CSS links

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+    return (
+        <Html>
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+            </Head >
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from '@src/partials/header';
 import Footer from '@src/partials/footer';
+import Head from 'next/head';
 
 export default function Home() {
     return (
         <React.Fragment>
+            <Head>
+                <title>Trang chủ</title>
+            </Head>
+
             <Header />
             <main></main>
             <Footer />
