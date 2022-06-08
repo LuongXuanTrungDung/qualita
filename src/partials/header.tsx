5bad7e11ba52789905b2a31953cb6971a7becbde
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2022-06-08 22:41:09 +0700

    Edit: Header and Color theme

--- src/partials/header.tsx
+++ src/partials/header.tsx
@@ -1,16 +1,15 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import { ThemeContext } from '@src/contexts/themeContext';
-import { ContactModalContext } from '@src/contexts/contactModalContext';
 
 export default function Header() {
     const { theme, setTheme } = useContext(ThemeContext);
-    const { setModalState } = useContext(ContactModalContext);
     const [isToggled, setIsToggled] = useState(false);
+    const headerRef = useRef<HTMLElement>(null);
 
-    const pages = ['Trang chủ', 'Giới thiệu', 'Dịch vụ'];
+    const pages = ['Trang chủ', 'Giới thiệu', 'Dịch vụ', 'Liên hệ'];
 
-    useEffect(() => {
+    useEffect(()    => {
         setIsToggled(theme === 'dark');
     }, []);
 
@@ -19,38 +18,26 @@ export default function Header() {
     }, [isToggled])
 
     return (
-        <header className="py-4 px-6 text-white bg-light dark:bg-dark">
+        <header ref={headerRef} className="py-4 px-6 sticky absolute top-0 left-0">
             <nav className="flex">
                 <ul className="flex mr-auto py-2">
-                    <li className="mr-2">
+                    <li className="mr-2 flex items-center">
                         <Link href="/" className="flex items-center">
-                            {theme === 'dark' ? (
-                                <img src="/logo-dark.svg" alt="Logo" className="w-6 h-6" />
-                            ) : (
-                                <img src="/logo-light.svg" alt="Logo" className="w-6 h-6" />
-                            )}
+                            <img src="/logo.svg" alt="Logo" className="w-6 h-6" />
                         </Link>
                     </li>
                     {pages.map((page, index) => (
-                        <li key={index} className="mx-2 flex items-center hover:bg-shade dark:hover:bg-shade rounded-md px-3 py-1">
+                        <li key={index} className="mx-2 flex items-center hover:text-primary dark:text-white dark:hover:bg-primary rounded-md px-3 py-1">
                             <Link href="/">{page}</Link>
                         </li>
                     ))}
-                    <li className="mx-2">
-                        <button
-                            onClick={setModalState.bind(null, true)}
-                            className="bg-dark dark:bg-light hover:bg-shade dark:hover:bg-shade rounded-md px-3 py-1"
-                        >
-                            Liên hệ
-                        </button>
-                    </li>
                 </ul>
                 <ul className="flex ml-auto">
                     <label htmlFor="themeToggle" className="flex items-center cursor-pointer">
                         <div className="relative">
                             <input type="checkbox" id="themeToggle" checked={isToggled} className="sr-only peer" onChange={() => setIsToggled(!isToggled)} />
-                            <div className="peer-checked:bg-light bg-dark w-14 h-8 rounded-full">
-                                <i className={"text-white absolute left-2 top-2 w-4 h-4 fas " + (isToggled ? "fa-moon" : "fa-sun translate-x-6")}></i>
+                            <div className="peer-checked:bg-gray-700 bg-gray-300 w-14 h-8 rounded-full">
+                                <i className={"text-black dark:text-white absolute left-2 top-2 w-4 h-4 fas " + (isToggled ? "fa-moon" : "fa-sun translate-x-6")}></i>
                             </div>
                             <div className="peer-checked:translate-x-full absolute bg-white flex w-6 h-6 left-1 top-1 flex items-center justify-center rounded-full transition"></div>
                         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "light": "#0080FF",
-                "dark": "#191970",
-                "fade": "#87D3F8",
-                "shade": "#4D4DFF"
+                "primary": "#0080FF"
             },
         },
     },
