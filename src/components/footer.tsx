fce6341de38d44d95f58c1e7b20068bf6b042d19
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2023-07-16 13:44:14 +0700

    Add: a footer contains a link to my Github profile and a JSON for testing uploading

---
+++ src/components/footer.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react'
+import { Link, Stack, Typography } from '@mui/material'
+import { LanguageContext } from '@contexts/useLanguage'
+
+export default function Footer() {
+  const { translate } = useContext(LanguageContext)
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <Stack
+      component="footer"
+      sx={{ mt: 'auto', mb: 2 }}
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Typography sx={{ mr: 1 }}>{translate('common:copyright')}</Typography>
+      <Link href="https://github.com/LuongXuanTrungDung">
+        {translate('common:authorName')}
+      </Link>
+      <Typography sx={{ ml: 1 }}>@ {currentYear}</Typography>
+    </Stack>
+  )
+}
